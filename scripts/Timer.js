a8094c4256be4afb3ec8bde28e8e988d2b9ef0ce
AngelinaPanteley <panteley-00@mail.ru>
2017-11-03 16:06:31 +0300

    apply Airbnb style guide

--- scripts/Timer.js
+++ scripts/Timer.js
@@ -1,78 +1,94 @@
-//This class based on the following examples
-//http://info-comp.ru/obucheniest/302-javascript-clock-stopwatch.html
+// This class based on the following examples
+// http://info-comp.ru/obucheniest/302-javascript-clock-stopwatch.html
 class Timer {
-    constructor(form) {
-        this.base=60;
-        this.form=form;
-        this.clearСlock();
+  constructor(form) {
+    this.base = 60;
+    this.form = form;
+    this.clearСlock();
+  }
+
+  clearСlock() {
+    clearTimeout(this.clocktimer);
+    this.h = 1;
+    this.m = 1;
+    this.tm = 1;
+    this.s = 0;
+    this.ts = 0;
+    this.ms = 0;
+    this.init = 0;
+    this.readout = '00:00.00';
+    this.form.value = this.readout;
+  }
+
+  startTime() {
+    let cdateObj = new Date();
+    let t = (cdateObj.getTime() - this.dateObj.getTime())-(this.s*1000);
+
+    if (t > 999)
+      this.s++;
+
+    if (this.s >= (this.m * this.base)) {
+      this.ts = 0;
+      this.m++;
+    } else {
+      this.ts = parseInt((this.ms / 100) + this.s);
+      if (this.ts>=this.base)
+        this.ts = this.ts - ((this.m - 1) * this.base);
     }
 
-    clearСlock() { 
-        clearTimeout(this.clocktimer); 
-        this.h=1;this.m=1;this.tm=1;this.s=0;this.ts=0;this.ms=0; 
-        this.init=0;
-        this.readout='00:00.00'; 
-        this.form.value=this.readout; 
-    }
-    
-    startTime() { 
-        var cdateObj = new Date(); 
-        var t = (cdateObj.getTime() - this.dateObj.getTime())-(this.s*1000); 
-        if (t>999) 
-            this.s++;
-        if (this.s>=(this.m*this.base)) { 
-            this.ts=0; 
-            this.m++; 
-        } else { 
-            this.ts=parseInt((this.ms/100)+this.s); 
-            if(this.ts>=this.base)
-                this.ts=this.ts-((this.m-1)*this.base); 
-        } 
-        if (this.m>(this.h*this.base)) { 
-            this.tm=1; 
-            this.h++; 
-        } else { 
-            this.tm=parseInt((this.ms/100)+this.m); 
-            if(this.tm>=this.base) 
-                this.tm=this.tm-((this.h-1)*this.base); 
-        } 
-        this.ms = Math.round(t/10); 
-        if (this.ms>99) 
-            this.ms=0; 
-        if (this.ms==0) 
-            this.ms='00';
-        if (this.ms>0&&this.ms<=9) 
-            this.ms = '0'+this.ms;  
-        if (this.ts>0) { 
-            this.ds = this.ts; 
-            if (this.ts<10)
-                this.ds = '0'+this.ts; 
-        } else
-            this.ds = '00'; 
-        this.dm=this.tm-1; 
-        if (this.dm>0) { 
-            if (this.dm<10)
-                this.dm = '0'+this.dm;
-        } else 
-             this.dm = '00';  
-        this.readout = this.dm + ':' + this.ds + '.' + this.ms; 
-        this.form.value = this.readout; 
-        this.clocktimer = setTimeout(this.startTime.bind(this),1); 
+    if (this.m > (this.h * this.base)) {
+      this.tm = 1;
+      this.h++;
+    } else {
+      this.tm=parseInt((this.ms / 100) + this.m);
+      if (this.tm >= this.base)
+        this.tm = this.tm - ((this.h - 1) * this.base);
     }
 
-    start() { 
-        if (this.init==0){ 
-            this.clearСlock();
-            this.dateObj = new Date(); 
-            this.startTime(); 
-            this.init=1; 
-        }
+    this.ms = Math.round(t / 10);
+
+    if (this.ms > 99)
+      this.ms = 0;
+
+    if (this.ms == 0)
+      this.ms = '00';
+
+    if (this.ms > 0 && this.ms <= 9)
+      this.ms = `0${this.ms}`;
+
+    if (this.ts > 0) {
+      this.ds = this.ts;
+      if (this.ts < 10)
+        this.ds = `0${this.ts}`;
+    } else
+      this.ds = '00';
+
+    this.dm = this.tm - 1;
+
+    if (this.dm > 0) {
+      if (this.dm < 10)
+        this.dm = `0${this.dm}`;
+    } else
+      this.dm = '00';
+
+    this.readout = `${this.dm}:${this.ds}.${this.ms}`;
+    this.form.value = this.readout;
+    this.clocktimer = setTimeout(this.startTime.bind(this),1);
+  }
+
+  start() {
+    if (this.init == 0) {
+      this.clearСlock();
+      this.dateObj = new Date();
+      this.startTime();
+      this.init = 1;
     }
-    
-    stop() { 
-        if (this.init==1){ 
-            clearTimeout(this.clocktimer);
-            this.init=0;
-        } 
+  }
+
+  stop() {
+    if (this.init == 1) {
+      clearTimeout(this.clocktimer);
+      this.init = 0;
     }
+  }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener('DOMContentLoaded', function(event) {
 
   document.getElementById('play-btn').addEventListener('click', function( event ) {
-    let settings = readSettingsForm(document.forms.settings);
-    let currentGame = new Game(settings.theme, settings.difficulty);
+    const currentGame = new Game();
+    currentGame.readSettingsForm(document.forms.settings);
     currentGame.createBoard();
-    let timer = new Timer(document.timer.stopwatch);
-    timer.start();
 
     currentGame.board.addEventListener('click', function(evt) {
       currentGame.clickCard(evt);
@@ -14,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     document.getElementById('end-btn').addEventListener('click', function( event ) {
       currentGame.destroyBoard();
       currentGame.returnToWelcomeBoard();
-      timer.stop();
     });
 
     document.getElementById('new-start-btn').addEventListener('click', function( event ) {
@@ -24,21 +21,3 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
 });
-
-function readSettingsForm(form) {
-  let settings = {};
-  let theme = form.elements.theme;
-  let difficulty = form.elements.difficulty;
-  theme.forEach(function(elem){
-    if(elem.checked)
-      settings.theme = elem.id.slice(-1);
-  });
-  difficulty.forEach(function(elem){
-    if(elem.checked)
-      settings.difficulty = elem.id.slice(-1);
-  });
-  return settings;
-}
-
-
-
